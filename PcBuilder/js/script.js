c00fb68397c5eb5e6f474bdbabb3be1d1a8419d8
SENIAN <tomroe90h@gmail.com>
2014-12-16 12:46:19 +0100

    PcBuilder
    met Search van een JSON bestand.
    
    Nog toe te voegen, clickable json output maken en dat als een request
    verzenden naar de back-end en implementeren in design van Firat

---
+++ PcBuilder/js/script.js
@@ -0,0 +1,51 @@
+﻿/* Dit is de Module bij Angular is het zo dat je de module
+een soort superclass is van de controller. De module kan ook 
+afhankelijkheden hebben van andere modules is wel handig voor
+de toekomst. (die array[] is dus de dependencies) maar hebben
+we nu niet nodig.*/
+
+var getData = angular.module('getData', []);
+
+
+
+/* Dit is de suggestion search zoals je 
+onder JSON ziet hieruit filtert hij bij het zoeken 
+zie : <input type="text" data-ng-model="filter.name" /> in de body*/
+
+
+getData.controller("jsonGetController", function($http,$scope)
+{    
+    $http({
+        method: 'GET',
+        dataType:'jsonp',
+        url: 'http://localhost/jsn.php'
+
+}).success (function(response, index){
+       
+    $scope.products = response;
+    console.log(response)
+
+        
+})
+    
+    }
+)
+
+
+
+
+
+    
+
+
+    /* Hier wordt de function addComponent verder verwerkt.
+    Zoals je ziet geeft hij een $scope (welke bekend is in zowel
+    de index als de js. De scope ziet alles mits je in de 
+    <html ng-app="modulenaam" hij ziet het ook zonder maar toch beter.
+    <button data-ng-click="addComponent()">Voeg onderdeel toe</button>
+    Zoals te zien is in de index zie je de bovenstaande code.
+    $scope houd in ik zoek in mijn scope de addComponent en dit is een
+    functie die het volgende moet doen {    
+    */
+
+   
---
+++ PcBuilder/jsn.php
@@ -0,0 +1,11 @@
+[
+           {"name": "Intel Core I7", "vendor": "Intel", "socket": "1155" },
+           {"name": "Intel Core I5", "vendor": "Intel", "socket": "1155" },
+           {"name": "Intel Core I3", "vendor": "Intel", "socket": "1155" },
+           {"name": "Intel Core 2 duo", "vendor": "Intel", "socket": "1155" },
+           {"name": "Intel Pentium D", "vendor": "Intel", "socket": "1155" },
+           {"name": "Geforce GTX 780", "vendor": "Gigabyte", "PCIE": "PCI 2.0" },
+           {"name": "Geforce GTX 560", "vendor": "Asus", "PCIE": "PCI-E 2.0" },
+           {"name": "Geforce GTX 550", "vendor": "Gforce", "PCIE": "PCI-E 2.0" },
+           {"name": "Geforce GTX", "vendor": "Nvidia", "PCIE": "PCI-E 2.0" }
+]
